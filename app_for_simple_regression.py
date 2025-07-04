d412dc88eb656f636a13e7975b919d05c98a744d
Jose80010 <107713614+Jose80010@users.noreply.github.com>
2025-07-04 12:05:42 -0600

    Update app_for_simple_regression.py

--- app_for_simple_regression.py
+++ app_for_simple_regression.py
@@ -17,12 +17,13 @@ st.title("📈 Simple Linear Regression Interactive Plot")
 st.write("Created by Dr Jose Islas")
 # Generate sample data
 np.random.seed(42)
-x_data = np.linspace(-5, 10, 50) # Changed x_data range to include negative values
+#x_data = np.linspace(-5, 10, 50) # Changed x_data range to include negative values
+x_data = np.array([150, 160, 170, 180, 190, 200])
 true_slope = 2.5
 true_intercept = 1.0
-noise = np.random.normal(0, 2, size=x_data.shape)
-y_data = true_slope * x_data + true_intercept + noise
-
+#noise = np.random.normal(0, 2, size=x_data.shape)
+#y_data = true_slope * x_data + true_intercept + noise
+y_data = np.array([5.0, 5.5, 6.0,6.5, 7.0, 7.5, 200])
 
 
 # Hypothetical data for demonstration
